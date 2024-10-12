17d55b74f8eb128aa205b06ba8475596d2b2604c
cosmicsaurabh <saurabhyadavclg20@gmail.com>
2024-10-13 00:02:45 +0530

    introduced all pages required like CreateQuiz,TakeQuiz,UserResult,QuizList

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import { AuthProvider } from './context/AuthContext';
+import CreateQuiz from './components/app/pages/CreateQuiz';
+import QuizList from './components/app/pages/QuizList';
+import TakeQuiz from './components/app/pages/TakeQuiz';
+import UserResults from './components/app/pages/UserResults';
 import Header from './components/app/Header';
-
+import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './routes/PrivateRoute';
 import PublicRoute from './routes/PublicRoute';
 import ErrorPage from './components/app/ErrorPage';
@@ -20,6 +23,10 @@ const App = () => {
             <Route path="/login" element={<PublicRoute element={<Login />} />} />
             <Route path="/register" element={<PublicRoute element={<Register />} />} />
 
+            <Route path="/create-quiz" element={<PrivateRoute element={<CreateQuiz />} />} />
+            <Route path="/quizzes" element={<PrivateRoute element={<QuizList />} />} />
+            <Route path="/quiz/:id" element={<PrivateRoute element={<TakeQuiz />} />} />
+            <Route path="/user-results" element={<PrivateRoute element={<UserResults />} />} />
             <Route path="/" element={<PrivateRoute element={<HomePage />} />} />
 
             <Route path="*" element={<ErrorPage/>}/>
